5daa835ff7c9b5432ac7c153f379ccb04743870c
BANSI BASHA <bansibasha@gmail.com>
2026-09-11 22:12:48 +0530

    Update cookie settings for token management

--- server/controller/auth_controller.js
+++ server/controller/auth_controller.js
@@ -14,8 +14,8 @@ const googleAuth=async (req,res)=>{
 
         res.cookie("token",token,{
             httpOnly:true,
-            secure:false,
-            sameSite:"strict",
+            secure:True,
+            sameSite:"none",
             maxAge:7*24*60*60*1000
         })
        
@@ -31,8 +31,8 @@ const logout=async (req,res)=>{
     try {
          res.clearCookie("token",{
             httpOnly:true,
-            secure:false,
-            sameSite:"strict"})
+            secure:True,
+            sameSite:"none"})
             res.status(200).json({message:"logout successful"})
     } catch (error) {
         return res.status(500).json({message:`logout error${error}`})
